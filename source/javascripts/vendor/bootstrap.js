7c7e3caa00d69d9664f70701cbc06151d0f3eaeb
Regis <boudinot.regis@yahoo.com>
2016-08-30 18:35:47 -0600

    load all of bootstrap - add new lines at end of files for POSIX compliance

--- source/javascripts/vendor/bootstrap.js
+++ source/javascripts/vendor/bootstrap.js
@@ -1,13 +1,2 @@
 //= require jquery
-//= require bootstrap/affix
-//= require bootstrap/alert
-//= require bootstrap/button
-//= require bootstrap/carousel
-//= require bootstrap/collapse
-//= require bootstrap/dropdown
-//= require bootstrap/tab
-//= require bootstrap/transition
-//= require bootstrap/scrollspy
-//= require bootstrap/modal
-//= require bootstrap/tooltip
-//= require bootstrap/popover
+//= require bootstrap
